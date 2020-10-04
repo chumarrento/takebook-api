4bb0b757596df232089999ee65635da38464b287
Felipe Barros <felipebarros.dev@gmail.com>
2020-10-03 21:51:01 -0300

    update(new-message): changed channels that receive new message event

--- app/Events/NewChatMessage.php
+++ app/Events/NewChatMessage.php
@@ -14,15 +14,23 @@ class NewChatMessage implements ShouldBroadcast
 	public $message;
 	public $room_id;
 
+	private $buyerChannel;
+	private $advertiserChannel;
+
 	public function __construct($message, $room_id)
 	{
 		$this->message = $message;
 		$this->room_id = $room_id;
+
+		$room = $message->room();
+
+		$this->buyerChannel = "userID{$room->buyer_id}";
+		$this->advertiserChannel = "userID{$room->advertiser_id}";
 	}
 
 	public function broadcastOn()
 	{
-		return ['room'.$this->room_id];
+		return [$this->advertiserChannel, $this->buyerChannel];
 	}
 
 	public function broadcastAs()
