dd27032a9e84ad381671ef3f0fb22b457e9a7ff2
Lucas Pereira <lucasp28.contato@gmail.com>
2020-10-03 23:34:35 -0300

    fix: register event on service provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -21,6 +21,8 @@ class EventServiceProvider extends ServiceProvider
 		'App\Events\NewChatMessage' => [
 			'App\Listeners\MessageChatListener',
 		],
-		'App\Events\NewChatRoom' => [],
+		'App\Events\NewChatRoom' => [
+			
+		],
     ];
 }
